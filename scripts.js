f8395900850d40a9d80f5e1eb5b00eed1b2f77d0
Karol Poplawski <Karol Poplawski>
2016-10-03 17:07:28 +0200

    js series playground

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+var greenElephants = "Zielone słonie";
+var ElephantsUpperCased = greenElephants.toUpperCase();
+
+var animal = text.replace('Papugi', ElephantsUpperCased);
+console.log(text.substr(text.length/2));
